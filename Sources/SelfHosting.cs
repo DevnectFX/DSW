415e126b5c36e79efd125f7b5acfa93fd060f10a
spowner <spowner@devnect.com>
2013-06-26 16:43:38 +0900

    default

---
+++ Sources/SelfHosting.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DSW
+{
+	public class SelfHosting
+	{
+		public SelfHosting ()
+		{
+		}
+
+		public static void Main(String[] args)
+		{
+			Console.WriteLine(args.Length);
+		}
+	}
+}
+
