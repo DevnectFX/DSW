dd3136e3d3aac8958267faf54cec9b97504d2d51
spowner <spowner@devnect.com>
2013-12-03 18:04:16 +0900

    2013-12-03

---
+++ Sources/FormatterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DSW.Responses;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DSW
+{
+    public static class FormatterExtensions
+    {
+        public static Response FromStream(this IResponseFormatter formatter, Stream stream, string filename, string contentType)
+        {
+            return new StreamResponse(() => stream, filename, contentType);
+        }
+    }
+}
---
+++ Sources/Responses/StreamResponse.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace DSW.Responses
+{
+    public class StreamResponse : Response
+    {
+        private Stream source;
+
+
+        public StreamResponse(Func<Stream> source, string filename, string contentType = null)
+        {
+            this.Contents = GetResponseBodyDelegate(source);
+            this.ContentType = contentType == null ? MimeTypes.GetMimeType(filename) : contentType;
+            this.StatusCode = HttpStatusCode.OK;
+            this.Headers["Content-Disposition"] = "attachment; filename=\"" + filename + "\"";
+        }
+
+        private Action<Stream> GetResponseBodyDelegate(Func<Stream> sourceDelegate)
+        {
+            return stream =>
+            {
+                using (this.source = sourceDelegate.Invoke())
+                {
+                    this.source.CopyTo(stream);
+                }
+            };
+        }
+    }
+}
